e3401ff0058656ec6495b88a6ac60287794eb5b7
ddinggu <officerudwls@gmail.com>
2023-06-25 03:42:45 +0900

    init

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "eks_vpc" {
   source = "./modules/vpc"
 
-  vpc_name        = "5am-eks-cluster-vpc"
+  vpc_name        = "<VPC 명>"
   vpc_cidr        = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
@@ -10,7 +10,7 @@ module "eks_vpc" {
 module "eks" {
   source = "./modules/eks_cluster"
 
-  cluster_name    = "5am-eks-cluster"
+  cluster_name    = "<EKS 클러스터 명>"
   cluster_version = "1.26"
   vpc_id          = module.eks_vpc.vpc_id
   subnet_ids      = module.eks_vpc.subnet_ids
@@ -19,7 +19,7 @@ module "eks" {
 module "web" {
   source = "./modules/web"
 
-  frontend_bucket_name = "5am-dev-web"
+  frontend_bucket_name = "<S3 웹 사이트 버킷 명>"
   index_document       = "index.html"
   error_document       = "index.html"
 }
