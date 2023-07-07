bb4a1c0124676fa2d6c267aa17a3a24627041be2
ddinggu <officerudwls@gmail.com>
2023-07-07 15:45:04 +0900

    리소스 최소 프로비저닝 시 cluster data resource를 불러오지 못하는 오류 수정

--- modules/eks_cluster/data.tf
+++ modules/eks_cluster/data.tf
@@ -2,10 +2,14 @@ data "aws_caller_identity" "available" {}
 
 data "aws_eks_cluster" "this" {
   name = module.eks.cluster_name
+
+  depends_on = [module.eks.cluster_endpoint]
 }
 
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
+
+  depends_on = [module.eks.cluster_endpoint]
 }
 
 ## karpenter ECR 이미지를 가져오기 위한 Auth token 발행(한국 리전 발행불가)
